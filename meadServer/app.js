befd3c704da146ebe67f4dd380a2b7d084114c3d
Brendan Nagle <bf.nagle@gmail.com>
2018-04-13 19:57:05 -0700

    bees

--- meadServer/app.js
+++ meadServer/app.js
@@ -52,7 +52,7 @@ const pool = new Pool({
   ssl: true
 });
 
-app.get('/db', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM "BatchData"');
