3e1b9613cfafc9dc00b9a0abbd38f38ffd0e76cd
Brendan Nagle <bf.nagle@gmail.com>
2018-04-21 16:21:32 -0700

    Integrating with React

--- meadServer/app.js
+++ meadServer/app.js
@@ -10,8 +10,6 @@ const client = new Client({
   ssl: true,
 });
 
-require("babel-core").transform("code", options);
-
 var batchNames = [];
 var selectedBatch;
 
@@ -32,44 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-
-// class BatchButtons extends React.Component
-// {
-// 	// constructor(props)
-// 	// {
-// 	// 	super(props);
-// 	// 	this.state =
-// 	// 	{
-// 	// 		buttons: [
-// 	// 			{
-// 	// 				text: '';
-// 	// 			}
-// 	// 		],
-// 	// 	}
-// 	// }
-
-// 	render()
-// 	{
-// 		const buttons = batchNames.map( x =>
-// 		{
-// 			const desc = x;
-// 			return (
-// 				<li key = {x}>
-// 					<button onClick={() => clickHandler(x)}>{desc}</button>
-// 				</li>
-// 			);
-// 		});
-
-// 		render(
-// 			<div className="user-options">
-// 				<div className "button-group">
-// 					<div>{'Choose a set of batch data'}</div>
-// 					<ol> {buttons} </ol>
-// 				</div>
-// 			</div>
-// 		);
-// 	}
-// }
 
 app.get('/', async function(req, res)
 {
