54d204d238c0a2eef1cb88d58647e5e45289a221
Brendan Nagle <bf.nagle@gmail.com>
2018-04-21 20:53:20 -0700

    Trying to get things working again

--- meadServer/client/src/App.js
+++ meadServer/client/src/App.js
@@ -24,10 +24,17 @@ class App extends Component
       .then(batchNames => this.setState({ batchNames }));
   }
 
+  // getData = () =>
+  // {
+  //   fetch('/api/batchData')
+  //     .then(res => res.json())
+  //     .then(batchData => this.setState({ batchData }))
+  // }
+
   render()
   {
     const { batchNames } = this.state.batchNames;
-    const { batchData } = this.state.batchData;
+    // const { batchData } = this.state.batchData;
 
     return (
       <div className="App">
@@ -58,15 +65,15 @@ class App extends Component
               Get More
             </button>
 
-            <h1>Batch Data</h1>
-            <u1 className="batchData">
-            {
-              batchData.map((data, index) =>
-                <li key = {data.sample_time}>
-                  {data}
-                </li>
-            )}
-            </u1>
+            // <h1>Batch Data</h1>
+            // <u1 className="batchData">
+            // {
+            //   batchData.map((data, index) =>
+            //     <li key = {data.sample_time}>
+            //       {data}
+            //     </li>
+            // )}
+            // </u1>
           </div>
         ) : (
         // Render a helpful message otherwise
