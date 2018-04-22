f0fd64f748ca872ba1ec30c50adf881fe304ba77
Brendan Nagle <bf.nagle@gmail.com>
2018-04-21 20:35:26 -0700

    Updates

--- meadServer/client/src/App.js
+++ meadServer/client/src/App.js
@@ -1,23 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-var Select = require('react-select');
-
-
-var options = [
-  { value: 'one', label: 'One' },
-  { value: 'two', label: 'Two' }
-];
-
-function logChange(val) {
-  console.log("Selected: " + JSON.stringify(val));
-}
-
-<Select
-  name="form-field-name"
-  value="one"
-  options={options}
-  onChange={logChange}
-/>
 
 class App extends Component
 {
