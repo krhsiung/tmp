748c056d3da740c74af2b781a4f165cbcc484ad5
Brendan Nagle <bf.nagle@gmail.com>
2018-04-21 18:09:49 -0700

    Bringing express and react onto heroku

--- meadServer/app.js
+++ meadServer/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const generatePassword = require('password-generator');
 const PORT = process.env.PORT || 5000
 const { Client } = require('pg');
 const client = new Client({
@@ -43,9 +42,7 @@ app.get('/api/passwords', (req, res) => {
   const count = 5;
 
   // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+  const passwords = Array.from(Array(count).keys()).map(i => ("Item" + i);)
 
   // Return them as json
   res.json(passwords);
