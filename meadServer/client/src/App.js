cee5dae44e2ae341d4236cd355ac209ded698ada
Brendan Nagle <bf.nagle@gmail.com>
2018-04-21 20:37:53 -0700

    Updates

--- meadServer/client/src/App.js
+++ meadServer/client/src/App.js
@@ -26,7 +26,7 @@ class App extends Component
 
   render()
   {
-    const { batchNames } = this.state;
+    const { batchNames } = this.state.batchNames;
 
     return (
       <div className="App">
