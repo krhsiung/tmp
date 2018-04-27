5174d937b2da4d2cdf32a34cea7de62279f77721
Brendan Nagle <bf.nagle@gmail.com>
2018-04-27 14:24:50 -0700

    Fixing the graph to look better

--- meadServer/client/src/App.js
+++ meadServer/client/src/App.js
@@ -71,7 +71,7 @@ class App extends Component
             axes
             grid
             verticalGrid
-            yDomainRange={[30, 110]}
+            yDomainRange={[50, 90]}
             axisLabels={{x: 'Time', y: 'Temperature (F)'}}
             height={400}
             width={600}
